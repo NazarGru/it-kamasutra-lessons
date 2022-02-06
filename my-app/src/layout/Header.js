bc86e81c1f460743e10b55357e2898b7cb7282b5
Gru-boy <nazarogarishev333@gmail.com>
2022-02-06 18:43:19 +0200

    home router components

--- my-app/src/layout/Header.js
+++ my-app/src/layout/Header.js
@@ -3,12 +3,14 @@ import React , {Component} from 'react'
 class Header extends Component {
     render(){
       return(
-          
+
         <header className='header'>
             <div className='container'>
-            Header
+            Heade
             </div>
          </header>
+
+
       )
     }
   }
